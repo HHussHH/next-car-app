8f68bafc34461c1ddbb31128530272ed3a38f7d8
Nariman <abonent26pg@mail.ru>
2023-04-08 20:28:20 +0300

    added fetch data

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,12 @@ import { FC, PropsWithChildren } from "react";
 import Header from "./header/Header";
 import Meta from "../seo/Meta";
 import { IMeta } from "../seo/meta.interface";
+import Footer from "./Footer";
+import dynamic from "next/dynamic";
 
+const DynamicFooter = dynamic(() => import("./Footer"), {
+  ssr: false,
+});
 const Layout: FC<PropsWithChildren<IMeta>> = ({
   children,
   title,
@@ -12,6 +17,7 @@ const Layout: FC<PropsWithChildren<IMeta>> = ({
     <Meta title={title} description={description}>
       <Header />
       <main>{children}</main>
+      <DynamicFooter />
     </Meta>
   );
 };
--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react";
 import Layout from "@/components/layout/Layout";
+import { ICarData } from "@/interfaces/car.interface";
+import CarItem from "@/components/ui/Car/CarItem";
 
-const Home: FC = () => {
+const Home: FC<ICarData> = ({ cars }) => {
   return (
     <Layout title="Home" description="we like to sale cars">
       <h1>Hello nextJs</h1>
+
+      {cars.length ? (
+        cars.map((car) => <CarItem key={car.id} car={car} />)
+      ) : (
+        <div>Cars not found</div>
+      )}
     </Layout>
   );
 };
